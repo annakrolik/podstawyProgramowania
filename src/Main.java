112363d2afc07be8d06788cd4e6af424fd6bff1c
annakrolik <84658264+annakrolik@users.noreply.github.com>
2021-06-13 14:50:35 +0200

    alpha test

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,12 @@ public class Main {
     public static void main (String args[]){
         String text = "Podstawy_programowania_JAVA";
         char nazwa1 = '1';
+        char alpha = '\u03B1';
 
         System.out.println(Character.isDigit(nazwa1));
         Character.isDigit(nazwa1);
         Character.isDigit(nazwa1);
+        System.out.println(alpha);
 
 
     }
