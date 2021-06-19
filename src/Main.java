ad4df5623d3c31cd6911f58b9cd1a16451a4d604
annakrolik <84658264+annakrolik@users.noreply.github.com>
2021-06-19 11:44:18 +0200

    motorcycle

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,18 @@ public class Main {
     public static void main (String args[]){
 
     Car Opel = new Car(2, 3.2, 5 );
-    Car Renault = new Car("Megane", "blue", 1999, "petrol");
+    Car Renault = new Car("Megane", "blue", 2000, "petrol");
 
     Opel.calculateMaximumSpeed();
 
     Renault.checkiffrom2000();
 
+    motorcycle Kawasaki = new motorcycle(8000, "Ninja", 0.3,2015, 'M');
+    Kawasaki.getPrice();
+    Kawasaki.setPrice(12000);
+    Kawasaki.getPrice();
+
+    Kawasaki.printMotorcycleInformation();
 
     }
 }
